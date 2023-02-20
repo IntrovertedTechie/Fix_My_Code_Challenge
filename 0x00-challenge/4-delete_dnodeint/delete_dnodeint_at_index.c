85ca31b8ad86ff82ee5c14219fa786a5adc2e690
Introvertedtechie <zillionaire234@gmail.com>
2023-02-20 16:05:30 +0100

    fmc

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -9,45 +9,43 @@
  *
  * Return: 1 on success, -1 on failure
  */
+/**
+ * delete_dnodeint_at_index - Deletes a node at a given position
+ *
+ * @head: A pointer to a pointer to the first element of the list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 if success, -1 otherwise
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current, *tmp;
 	unsigned int i;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
 
 	current = *head;
-	for (i = 0; i < index && current != NULL; i++)
-	{
-		current = current->next;
-	}
-	if (i != index)
-	{
-		return (-1);
-	}
-
 	if (index == 0)
 	{
 		*head = current->next;
-		if (*head != NULL)
-		{
-			(*head)->prev = NULL;
-		}
+		if (current->next)
+			current->next->prev = NULL;
 		free(current);
+		return (1);
 	}
-	else
-	{
-		tmp = current->prev;
-		tmp->next = current->next;
-		if (current->next != NULL)
-		{
-			current->next->prev = tmp;
-		}
-		free(current);
-	}
+
+	for (i = 0; current && i < index - 1; i++)
+		current = current->next;
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	tmp = current->next;
+	current->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = current;
+	free(tmp);
 
 	return (1);
 }
