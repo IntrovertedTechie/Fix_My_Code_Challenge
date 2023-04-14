55b604beca9f0f80432c919a3f2db196e8a1cc94
Introvertedtechie <zillionaire234@gmail.com>
2023-04-14 14:52:41 +0000

    zz

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a Flask application that provides an API for interacting with data.
+Web server 
 """
-
-import json
-from flask import Flask, make_response
 from api.v1.views import app_views
-
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,15 +11,10 @@ app.register_blueprint(app_views)
 
 @app.errorhandler(404)
 def not_found(error):
-    """
-    JSON 404 page
-    """
-    response_dict = {"error": "Not found"}
-    response = make_response(json.dumps(response_dict, indent=4), 404)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    """ json 404 page """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    
+    # python -m api.v1.app 
+    app.run(host="0.0.0.0", port=5000)
--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -4,27 +4,39 @@ User class
 """
 
 class User():
-    """ Documentation """
+    """ 
+    This class represents a user with an email address.
+    """
 
     def __init__(self):
-        """ Documentation """
+        """ 
+        Initializes a new User object with an email address set to None.
+        """
         self.__email = None
+
+    @property
+    def email(self):
+        """ 
+        Gets the email address of the user.
+
+        Returns:
+        The email address of the user as a string.
+        """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ 
+        Sets the email address of the user.
+
+        Args:
+        value (str): The email address to set.
+
+        Raises:
+        TypeError: If the value is not a string.
+        """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
-if __name__ == "__main__":
 
-    u = User()
-    u.email = "john@snow.com"
-    print(u.email)
